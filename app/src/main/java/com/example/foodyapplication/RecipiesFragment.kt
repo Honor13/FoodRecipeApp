11df9021def500a4e7107ab0aff8a9d9f13e90d6
Honor13 <onury.karaduman@gmail.com>
2023-11-25 18:40:52 +0300

    Shimmer and RV design OK

--- app/src/main/java/com/example/foodyapplication/RecipiesFragment.kt
+++ app/src/main/java/com/example/foodyapplication/RecipiesFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.foodyapplication.databinding.FragmentRecipiesBinding
 
+private lateinit var binding:FragmentRecipiesBinding
 class RecipiesFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipies, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_recipies, container, false)
+
+
+        binding.shimmerRV.showShimmer(true)
+
+        return binding.root
     }
 
 
